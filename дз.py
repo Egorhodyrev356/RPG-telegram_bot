733f3171eb8eed3bf10b910122b04f174de1c527
Egor_Hody <egorhodyrev37@mail.ru>
2025-11-06 18:58:23 +0300

    Начало работы над проектом

--- дз.py
+++ дз.py
@@ -10,18 +10,3 @@ specials = input("Нужны ли спецсимволы? (да/нет): ").lowe
 # Базовый набор символов — буквы в обоих регистрах
 characters = string.ascii_letters
 
-# Если нужны числа — добавим к базовым допустимым значениям числовые знаки
-if digits:
-    characters += string.digits
-
-# Если требуются спецсимволы – добавим их тоже!
-if specials:
-    characters += "!@#$%&*"
-
-password_list=[]
-for x in range(length):
-     password_list.append(random.choice(characters))
-
-password="".join(password_list)
-
-print(f"Сгенерированный пароль: {password}")
