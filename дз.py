7bd818689a8e2a4656b0f31b872ccdfebfbfd08f
Egor_Hody <egorhodyrev37@mail.ru>
2025-11-06 18:56:58 +0300

    Поменял символы из презентации

---
+++ дз.py
@@ -0,0 +1,27 @@
+import random
+import string
+
+
+# Запрашиваем параметры у пользователя
+length = int(input("Введите длину пароля (от 6 до 20): "))
+digits = input("Нужны ли цифры? (да/нет): ").lower() == "да"
+specials = input("Нужны ли спецсимволы? (да/нет): ").lower() == "да"
+
+# Базовый набор символов — буквы в обоих регистрах
+characters = string.ascii_letters
+
+# Если нужны числа — добавим к базовым допустимым значениям числовые знаки
+if digits:
+    characters += string.digits
+
+# Если требуются спецсимволы – добавим их тоже!
+if specials:
+    characters += "!@#$%&*"
+
+password_list=[]
+for x in range(length):
+     password_list.append(random.choice(characters))
+
+password="".join(password_list)
+
+print(f"Сгенерированный пароль: {password}")
