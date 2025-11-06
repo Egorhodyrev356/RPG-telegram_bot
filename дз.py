82b606dd8137dcadabc9d84c0b3744396d09c663
Egor_Hody <egorhodyrev37@mail.ru>
2025-11-06 19:01:16 +0300

    Начало работы над проектом

--- дз.py
+++ дз.py
@@ -1,6 +1,6 @@
 import random
 import string
-
+from string import digits
 
 # Запрашиваем параметры у пользователя
 length = int(input("Введите длину пароля (от 6 до 20): "))
@@ -10,3 +10,18 @@ specials = input("Нужны ли спецсимволы? (да/нет): ").lowe
 # Базовый набор символов — буквы в обоих регистрах
 characters = string.ascii_letters
 
+# Если нужны числа — добавим к базовым допустимым значениям числовые знаки
+if digits:
+    characters += string.digits
+
+# Если требуются спецсимволы – добавим их тоже!
+if specials:
+    characters += "!@#$%&*"
+
+password_list=[]
+for x in range(length):
+     password_list.append(random.choice(characters))
+
+password="".join(password_list)
+
+print(f"Сгенерированный пароль: {password}")
